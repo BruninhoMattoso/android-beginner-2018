f87b7dd7808bb66915ba2b95346f96398d9abc3b
Bruno Gonçalves <bruno.goncalves@dextra-sw.com>
2018-11-09 01:00:17 -0200

    1.5.3 - Defaul Intent

--- app/src/main/java/br/app/dextra/android_begginer_2018/demo/SplashActivity.java
+++ app/src/main/java/br/app/dextra/android_begginer_2018/demo/SplashActivity.java
@@ -1,8 +1,11 @@
 package br.app.dextra.android_begginer_2018.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import br.app.dextra.android_begginer_2018.MainActivity;
 import br.app.dextra.android_begginer_2018.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +14,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openMainActivity();
+            }
+        }, 3000);
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
