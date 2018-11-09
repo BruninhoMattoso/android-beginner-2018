9ca4e65d36e6f129d8c2997c5ffefa2eec383d32
Bruno Gonçalves <bruno.goncalves@dextra-sw.com>
2018-11-09 00:27:32 -0200

    1.4.1 - Looking View in Java

--- app/src/main/java/br/app/dextra/android_begginer_2018/MainActivity.java
+++ app/src/main/java/br/app/dextra/android_begginer_2018/MainActivity.java
@@ -1,7 +1,10 @@
 package br.app.dextra.android_begginer_2018;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView dextraPng = findViewById(R.id.dextra_png);
+
+        // Mudando a visualizacao da imagem informada no xml acima
+        dextraPng.setVisibility(View.VISIBLE);
+
+        // Adicionando um evento de click na imagem
+        dextraPng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clique da imagem", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
